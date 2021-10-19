58b12672da9718b04959886a70b7c730693dac7e
Mohd Faraz <androiabledroid@gmail.com>
2021-10-19 13:02:43 +0530

    media player: fixes
     * Added Sample music
    
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,32 @@
 let slider = document.querySelector(".tracker")
-console.log(slider)
+let playbtn = document.querySelector(".play")
+let pausebtn = document.querySelector(".pause");
+let prevbtn = document.querySelector(".previous");
+let nextbtn = document.querySelector(".next");
 let menu = document.getElementById('menu');
+let audio = document.getElementById('audio')
 let mShowing = false;
 menu.addEventListener('click', () => {
-    if (!mShowing)
+    if (!mShowing) {
         slider.classList.add("translate");
+        menu.classList.add('open');
+    }
     else
-        slider.classList.remove("translate");
+        {slider.classList.remove("translate");
+    menu.classList.remove('open');}
     mShowing = !mShowing;
 });
+
+playbtn.addEventListener('click', () => {
+    var data = playbtn.className;
+    if (data.search('pause') == -1)
+    {
+        playbtn.classList.remove('play')
+        playbtn.classList.add('pause')
+        audio.play()
+    } else {
+        playbtn.classList.remove('pause')
+        playbtn.classList.add('play')
+        audio.pause()
+    }
+})
