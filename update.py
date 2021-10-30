3ef9d8284cb0e8117b378f2d8d3fa39183a74153
Mohd Faraz <androiabledroid@gmail.com>
2021-10-31 02:43:40 +0530

    update.py: do not read json
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- update.py
+++ update.py
@@ -8,17 +8,18 @@ onlyfiles = listdir("tracks/")
 
 data = []
 for i in onlyfiles:
-	metadata = stagger.read_tag("tracks/" + i)
-	image_data = metadata['APIC'][0].data
+	if i != "tracks.json":
+		metadata = stagger.read_tag("tracks/" + i)
+		image_data = metadata['APIC'][0].data
 
-	datauri = "data:{};base64,{}".format(metadata['APIC'][0].mime,str(base64.b64encode(image_data))[2:-1])
-	item = {
-			"cover": str(datauri),
-			"album": str(metadata.album),
-			"song": str(metadata.title),
-			"artist": str(metadata.artist)
-		}
+		datauri = "data:{};base64,{}".format(metadata['APIC'][0].mime,str(base64.b64encode(image_data))[2:-1])
+		item = {
+				"cover": str(datauri),
+				"album": str(metadata.album),
+				"song": str(metadata.title),
+				"artist": str(metadata.artist)
+			}
 
-	data.append(item)
+		data.append(item)
 with open("tracks/tracks.json", "w") as outfile:
 	outfile.write(json.dumps(data, indent=4))
