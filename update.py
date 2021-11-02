be4a3440a3e79a4a78d0ad5b123ba35df4c3470c
Mohd Faraz <androiabledroid@gmail.com>
2021-11-03 02:37:55 +0530

    update: Update more code for unwanted songs
     * using audio-metadata instead off stagger
     Reason: Supports more codecs
    
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- update.py
+++ update.py
@@ -1,8 +1,10 @@
 import json
-import stagger
+import audio_metadata as am
+import tbm_utils as tbm
 import base64
 import sys
 from os import listdir
+from os import remove
 
 onlyfiles = listdir("tracks/")
 
@@ -11,17 +13,17 @@ for i in onlyfiles:
 	if i != "tracks.json":
 		item = {}
 		try:
-			metadata = stagger.read_tag("tracks/" + i)
+			metadata = am.load("tracks/" + i)
 			item = {
 				"file": str("tracks/" + i),
 				"cover": "",
-				"album": str(metadata.album),
-				"song": str(metadata.title),
-				"artist": str(metadata.artist)
+				"album": str(metadata['tags'].album)[1:-1][1:-1],
+				"song": str(metadata['tags'].title)[1:-1][1:-1],
+				"artist": str(metadata['tags'].artist)[1:-1][1:-1]
 			}
 			try:
-				image_data = metadata['APIC'][0].data
-				datauri = "data:{};base64,{}".format(metadata['APIC'][0].mime,str(base64.b64encode(image_data))[2:-1])
+				image_data = metadata['pictures'][0].data
+				datauri = "data:{};base64,{}".format(metadata['pictures'][0].mime_type,str(base64.b64encode(image_data))[2:-1])
 				item["cover"] = str(datauri)
 			except:
 				item["cover"] = "/resources/Record.png"
@@ -32,7 +34,7 @@ for i in onlyfiles:
 					item["artist"] = "Unknown"
 				if item["song"] == "":
 					item["song"] = str(i)
-		except:
+		except AttributeError:
 			item = {
 				"file": str("tracks/" + i),
 				"cover": "/resources/Record.png",
@@ -40,6 +42,10 @@ for i in onlyfiles:
 				"song": str(i),
 				"artist": "Unknown"
 			}
+		except:
+			print("Removed Malicious file " + i)
+			remove("tracks/" + i)
+			continue
 		data.append(item)
 with open("tracks/tracks.json", "w") as outfile:
 	outfile.write(json.dumps(data, indent=4))
